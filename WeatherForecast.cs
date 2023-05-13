34d15a868eccb241aa193661aaf11b07e4227d0d
Felipe <felipe8ffs@gmail.com>
2023-05-13 20:12:26 -0300

    Initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Atv1Astrologia
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
